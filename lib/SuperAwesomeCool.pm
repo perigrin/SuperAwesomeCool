edf684d1236258f4a6b1f2e116b988cef5026b85
Chris Prather <chris@prather.org>
2011-06-23 03:23:45 -0400

    remove the kioku-bits for now

--- lib/SuperAwesomeCool.pm
+++ lib/SuperAwesomeCool.pm
@@ -4,76 +4,6 @@ use Bread::Board::Declare;
 use Plack::Builder;
 use Plack::Middleware::Magpie;
 
-has dsn => (
-    isa   => 'Str',
-    is    => 'ro',
-    value => 'dbi:SQLite:site.db'
-);
-
-has kioku => (
-    isa          => 'SuperAwesomeCool::KiouDB',
-    is           => 'ro',
-    lifecycle    => 'Singleton',
-    dependencies => ['dsn'],
-);
-
-has template_root => (
-    isa   => 'Str',
-    is    => 'ro',
-    value => './root/src',
-);
-
-# if we can allow Instances of resources to be added to the pipeline
-# we can change this to use constructor injection
-#
-# has user_resource => (
-#     isa          => 'SuperAwesomeCool::Resource::User',
-#     is           => 'ro',
-#     dependencies => ['kioku'],
-# );
-#
-# and potentially the "infer" magic that I don't really understand
-
-has reservation_resource => (
-    isa   => 'ArrayRef',
-    is    => 'ro',
-    block => sub {
-        my $s = shift;
-        [
-            'SuperAwesomeCool::Resource::Reservation',
-            { kioku => $s->param('kioku') }
-        ];
-    },
-    dependencies => ['kioku'],
-);
-
-has user_resource => (
-    isa   => 'ArrayRef',
-    is    => 'ro',
-    block => sub {
-        my $s = shift;
-        [ 'SuperAwesomeCool::Resource::User', { kioku => $s->param('kioku') } ];
-    },
-    dependencies => ['kioku'],
-);
-
-has template_resource => (
-    isa   => 'ArrayRef',
-    is    => 'ro',
-    block => sub {
-        my $s = shift;
-        [
-            'SuperAwesomeCool::Resource::Template',
-            {
-                root      => $s->param('template_root'),
-                extension => '.tt2',
-                dir_index => 'index.tt2',
-            }
-        ];
-    },
-    dependencies => ['template_root'],
-);
-
 has resource_map => (
     isa   => 'HashRef',
     is    => 'ro',
@@ -81,15 +11,11 @@ has resource_map => (
         my $s = shift;
         {
             '/static'      => ['Magpie::Resource::File', { root => './root/static'} ],
-            '/user'        => $s->param('user_resource'),
-            '/reservation' => $s->param('reservation_resource'),
             '/'            => $s->param('template_resource'),
         };
     },
     dependencies => [
         qw(
-          user_resource
-          reservation_resource
           template_resource
           )
     ],
